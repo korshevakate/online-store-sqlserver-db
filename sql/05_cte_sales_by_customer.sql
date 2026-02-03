2fb934c3298e7dcd5f3e80309ea06741ebae006b
Yekaterina <korshevakate@gmail.com>
2026-02-03 11:19:59 +0500

    Добавление CTE

---
+++ sql/05_cte_sales_by_customer.sql
@@ -0,0 +1,29 @@
+USE online_store;
+GO
+
+/*
+CTE: Общая сумма покупок по каждому клиенту
+Используется для аналитики продаж
+*/
+
+WITH CustomerSales AS (
+    SELECT
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        SUM(oi.quantity * p.price) AS total_amount
+    FROM Customers c
+    JOIN Orders o
+        ON c.customer_id = o.customer_id
+    JOIN OrderItems oi
+        ON o.order_id = oi.order_id
+    JOIN Products p
+        ON oi.product_id = p.product_id
+    GROUP BY
+        c.customer_id,
+        c.first_name,
+        c.last_name
+)
+SELECT *
+FROM CustomerSales
+ORDER BY total_amount DESC;
