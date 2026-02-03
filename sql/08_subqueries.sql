16b10f3b46f48ec1c23dbdd62ccbe5e1e0edc15d
Yekaterina <korshevakate@gmail.com>
2026-02-03 14:14:04 +0500

    Добавила недостающие файлы

--- sql/08_subqueries.sql
+++
@@ -1,6 +0,0 @@
--- Пример уникальности и проверки
-ALTER TABLE Customers
-ADD CONSTRAINT UQ_Customers_Email UNIQUE(email);
-
-ALTER TABLE Products
-ADD CONSTRAINT CHK_Products_Price CHECK (price >= 0);
